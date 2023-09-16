3feff4ac4d7d26bb17e71e8c9f181fddd7401c2b
ranusingh1993 <ranusingh1993@gmail.com>
2023-09-16 22:33:55 +0530

    adding method to check db is up or not with test cases

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,13 +1,30 @@
 """
 Django command to wait for database to be avaialble. 
 """
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from typing import Any, Optional
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
-    def handle(self, *args: Any, **options):
-        pass
+    def handle(self, *args, **options):
+        """Entrypoint for command"""
+        self.stdout.write("waiting for databse")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases = ['default'])
+                db_up = True
+            except(Psycopg2OpError,OperationalError):
+                self.stdout.write("databse is unable, waiting 1 second")
+                time.sleep(1)
+        
+        self.stdout.write(self.style.SUCCESS('Database Avaialble'))
+        
+
     
