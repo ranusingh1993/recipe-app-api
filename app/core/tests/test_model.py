11b212752045e8f3d43b5efee1b5a2a04797b5e9
ranusingh1993 <ranusingh1993@gmail.com>
2023-09-17 01:03:01 +0530

    fixinf typos

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -29,7 +29,7 @@ class ModelTests(TestCase):
 
         ]
         for email,expected in sample_emails:
-            user = get_user_model.objects.create_user(email,'sample123')
+            user = get_user_model().objects.create_user(email,'sample123')
             self.assertEqual(user.email,expected)
 
 
